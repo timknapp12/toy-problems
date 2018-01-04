f8c5e2b402f6481c15b7b053cf4fe2679206272a
timknapp12 <timknapp12@gmail.com>
2018-01-04 14:39:01 -0700

    first commit

---
+++ javascript/level-one.js
@@ -0,0 +1,17 @@
+// What do you do when you see a JS toy problem?
+// 1- clarify what is being asked.
+// 2- look at or ask for sample data/ sample results.
+// 3- pseudo code- code broken down into regular English - list out the tools or methods that you’ll use.
+// 4- code
+// 5- come up with another solution
+
+// PROBLEM #1
+
+// Create a function to filter over the array called numbers and return a new array with only the even numbers. Make sure to use the arrow function combined with the filter method.
+// * Should use arrow function
+// * Should use filter method
+// var numbers = "TBD";
+
+var numbers = [1, 2, 3, 4];
+
+var evens = () => numbers.filter(e => e % 2 === 0);
