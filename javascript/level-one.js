62e4f09546c3b03e39d2a895e38a0361ccc40d65
timknapp12 <timknapp12@gmail.com>
2018-01-15 11:09:52 -0700

    map Hello

--- javascript/level-one.js
+++ javascript/level-one.js
@@ -5,7 +5,7 @@
 // 4- code
 // 5- come up with another solution
 
-// PROBLEM #1   Evens array
+                // PROBLEM #1   Evens array
 
 // Create a function to filter over the array called numbers and return a new array with only the even numbers. Make sure to use the arrow function combined with the filter method.
 // * Should use arrow function
@@ -16,7 +16,7 @@ var numbers = [1, 2, 3, 4];
 
 var evens = () => numbers.filter(e => e % 2 === 0);
 
-// PROBLEM #2  odd and even arrays
+            // PROBLEM #2  odd and even arrays
 
 // Create a function using the filter method that separates all even numbers and all odd numbers into separate arrays.
 //* [1, 2, 3, 4, 5, 6] should result in [1, 3, 5] and [2, 4, 6]
@@ -47,7 +47,7 @@ function separate(arr) {
     })
   } 
 
-  //     PROBLEM #3   reverse array from n
+  //                 PROBLEM #3   reverse array from n
   //Get the number n to return the reversed sequence from n to 1.
 
 // Example : n=5 >> [5,4,3,2,1]
@@ -66,7 +66,7 @@ const reverseSeq = (n) => {
 
   const reverseSeq = n => Array.from({length: n}, () => n--);
 
-  // PROBLEM #4
+                         // PROBLEM #4
 // Inside the startWithLetterA function use the built in filter method to filter over the array called strings and return a new array with only the strings that start with a letter "a". Make sure to use the arrow function combined with the filter method.
 // * The strings variable will be changed during tests.
 // * Should use arrow function
@@ -81,3 +81,23 @@ function sortStr(arr) {
 //the solution below is a bit more streamlined with more ES6 (additional arrow function) and a function expression
 
 var sortStr = arr => arr.filter(item => item[0] === 'a' || item[0] === 'A');
+
+             //   PROBLEM #5
+ // Inside the formalGreeting function use the built in .map method to map over the array called names and return a new array with "Hello, " appended to the beginning of each name. Make sure to use the arrow function combined with the map method.
+
+// * The names variable will be changed during tests.
+// * Should use map method
+
+var names = ['Jim', 'Tim', 'Joe', 'Josh']
+
+
+function formalGreeting(arr) {
+  return arr.map(e => "Hello " + e);
+}
+formalGreeting(names);
+
+// use of arrow functions, a template string, and function expression below:
+
+var formalGreeting = arr => arr.map(e => `Hello ${e}`);
+
+formalGreeting(names);
